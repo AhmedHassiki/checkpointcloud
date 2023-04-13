7125f938d4e1b9146763c5731663ab59daae74a9
Ahmed Hassiki <hassiki.services@gmail.com>
2023-04-13 07:46:30 +0100

    checkpointcloud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import IntroSection from './components/intro/Intro'
+import ContactSection from './components/contact-section/ContactSection'
+import MapSection from './components/map/Map' // import the map here
+import DisclaimerSection from './components/disclaimer/Disclaimer'
+import FooterSection from './components/footer/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const location = {
+  address: "Dacquoise, RR28, 8000 Gouvernorat Nabeul, Tunisie",
+  lat: 36.445084341968474,
+  lng: 10.715781623515763,
+};
+
+const App = () => (
+  <div className="App">
+    <IntroSection />
+    <ContactSection />
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+    <DisclaimerSection />
+    <FooterSection />
+  </div>
+)
 
 export default App;
